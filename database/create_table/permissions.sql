67bfc8b78cd3381f120719164b74d5e4cbe824c4
Hoa Nguyen <hoaannguyen07@tamu.edu>
2021-06-22 16:13:25 -0500

    Update permissions.sql

--- database/create_table/permissions.sql
+++ database/create_table/permissions.sql
@@ -1,7 +1,7 @@
 USE [warehouse_management]
 GO
 
-/****** Object:  Table [dbo].[permissions]    Script Date: 6/14/2021 9:46:05 PM ******/
+/****** Object:  Table [dbo].[permissions]    Script Date: 6/22/2021 4:12:03 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,34 +9,40 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[permissions](
-	[id] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
-	[action] [nvarchar](10) NOT NULL,
-	[object] [nvarchar](30) NOT NULL,
- CONSTRAINT [pk_permissions_id] PRIMARY KEY NONCLUSTERED 
+	[id] [uniqueidentifier] NOT NULL,
+	[action_id] [int] NOT NULL,
+	[object_id] [int] NOT NULL,
+ CONSTRAINT [PK_permissions_id] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
- CONSTRAINT [IX_permissions_action_object] UNIQUE NONCLUSTERED 
+ CONSTRAINT [UK_action_object] UNIQUE NONCLUSTERED 
 (
-	[object] ASC,
-	[action] ASC
+	[action_id] ASC,
+	[object_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[permissions] ADD  CONSTRAINT [DF_permissions_id]  DEFAULT (newid()) FOR [id]
+ALTER TABLE [dbo].[permissions] ADD  DEFAULT (newid()) FOR [id]
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'system generated id for specific permission' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'permissions', @level2type=N'COLUMN',@level2name=N'id'
+ALTER TABLE [dbo].[permissions]  WITH CHECK ADD  CONSTRAINT [FK_permissions_permission_actions] FOREIGN KEY([action_id])
+REFERENCES [dbo].[permission_actions] ([id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'type of action (create/read/update/delete)' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'permissions', @level2type=N'COLUMN',@level2name=N'action'
+ALTER TABLE [dbo].[permissions] CHECK CONSTRAINT [FK_permissions_permission_actions]
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'type of object (table) an action could be performed on' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'permissions', @level2type=N'COLUMN',@level2name=N'object'
+ALTER TABLE [dbo].[permissions]  WITH CHECK ADD  CONSTRAINT [FK_permissions_permission_objects] FOREIGN KEY([object_id])
+REFERENCES [dbo].[permission_objects] ([id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'permissions', @level2type=N'CONSTRAINT',@level2name=N'pk_permissions_id'
+ALTER TABLE [dbo].[permissions] CHECK CONSTRAINT [FK_permissions_permission_objects]
 GO
 
 

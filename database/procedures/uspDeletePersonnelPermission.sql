a9f5fb8c60f8d85c9523e2022dd68edacc35efeb
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-06-30 12:09:30 -0500

    updated deletepersonnelpermission usp to implement the linear verification format

--- database/procedures/uspDeletePersonnelPermission.sql
+++ database/procedures/uspDeletePersonnelPermission.sql
@@ -9,13 +9,14 @@ GO
 --	@auth nvarchar(50), -- username of the person requesting to delete personnel permission
 --	@response nvarchar(256) = '' OUTPUT
 
--- OUTPUT(S) (by precedence):
+-- OUTPUT(S) (by precedence) (SUCCESS WILL ALWAYS BE '0' BUT BE LOWEST ON THE PRECEDENCE LIST):
 --		1. ERROR: 'Username does not exist' -> @username is not a valid username in the database
 --		2. ERROR: 'Permission does not exist' -> [@action-@object] permission does not exist in the database
 --		3. ERROR: 'Personnel does not have this permission' -> @username does not have the [@action-@object] permission to begin with
 --		4. ERROR: 'Unauthorized to delete permission' -> @auth does not have the permission to perform this action 
 --		5. ERROR: ERROR_MESSAGE() -> error occurred during the DELETE operation of the personnel permission
---		5. 'SUCCESS' -> Successfully removed the [@action-@object] from @username on the personnel_permissions table
+--		6. ERROR: 'Error occurred during the DELETE operation' -> something went wrong during the DELETE operation from the personnel_permissions table but did not cause any errors (or it would've returned ERROR_MESSAGE() instead)
+--		0. 'SUCCESS' -> Successfully removed the [@action-@object] from @username on the personnel_permissions table
 
 
 CREATE OR ALTER PROCEDURE [dbo].[uspDeletePersonnelPermission]
@@ -23,95 +24,92 @@ CREATE OR ALTER PROCEDURE [dbo].[uspDeletePersonnelPermission]
 	@action NVARCHAR(10),
 	@object NVARCHAR(30),
 	@auth nvarchar(50),
-	@response nvarchar(256) = '' OUTPUT
+	@response nvarchar(256) = NULL OUTPUT
 AS
 BEGIN
-	SET NOCOUNT ON
+	SET NOCOUNT ON	
 
-	DECLARE @delete_personnel_permission_response nvarchar(256) = NULL
-
-	IF EXISTS(SELECT dbo.personnel.id FROM dbo.personnel WHERE username=@username)
+	-- check for existence of personnel b/c every permission given is given to a personnel in the personnel table
+	IF NOT EXISTS(SELECT dbo.personnel.id FROM dbo.personnel WHERE username=@username)
 	BEGIN
-		IF EXISTS(SELECT permissions.id FROM permissions
-					INNER JOIN dbo.permission_actions ON dbo.permissions.action_id = dbo.permission_actions.id
-					INNER JOIN dbo.permission_objects ON dbo.permissions.object_id = dbo.permission_objects.id
-					WHERE dbo.permission_actions.action = @action 
-					AND dbo.permission_objects.object = @object)
-		BEGIN
-				-- only delete permission from @username if @username has the [@action-@object] permission in the first place
-				DECLARE @personnel_has_permission_response nvarchar(3)
-
-				EXEC dbo.uspCheckPersonnelPermission
-					@username = @username,
-					@action = @action,
-					@object = @object,
-					@response = @personnel_has_permission_response OUTPUT
-
-
-				IF (@personnel_has_permission_response = 'YES')
-				BEGIN
-						-- check if @auth has the authority to create personnel
-						DECLARE @check_permissions_response nvarchar(3)
-						EXEC dbo.uspCheckPersonnelPermission
-							@username = @auth,
-							@action = 'delete',
-							@object = 'personnel_permissions',
-							@response = @check_permissions_response OUTPUT
-
-						-- if auth is granted, then go ahead with creating new personnel
-						IF (@check_permissions_response = 'YES')
-						BEGIN
-							BEGIN TRY
-								DELETE FROM dbo.personnel_permissions 
-								WHERE
-								personnel_id=(SELECT TOP 1 id FROM dbo.personnel WHERE username=@username) AND
-								permissions_id=(SELECT TOP 1 id FROM dbo.permissions 
-												WHERE action_id=(SELECT id FROM dbo.permission_actions WHERE action = @action) 
-												AND object_id=(SELECT id FROM dbo.permission_objects WHERE object = @object))
-
-								-- have to make sure permission is deleted from the database (in case no severe errors raised to push into CATCH block but database did not record properly)
-								EXEC dbo.uspCheckPersonnelPermission
-									@username = @username,
-									@action = @action,
-									@object = @object,
-									@response = @personnel_has_permission_response OUTPUT
-
-								IF (@personnel_has_permission_response = 'NO')
-								BEGIN
-									SET @delete_personnel_permission_response = 'SUCCESS'
-								END
-								ELSE
-								BEGIN						
-									DECLARE @message nvarchar(256) = NULL
-									SET @message = CONCAT('The DELETE operation was unable to remove the [', @action, '-', @object, '] permission from the username [', @username , ']')
-
-									RAISERROR(
-										@message, -- Message
-										16, -- Severity
-										1 -- State
-									)
-								END
-							END TRY
-							BEGIN CATCH
-								SET @delete_personnel_permission_response = ERROR_MESSAGE()
-							END CATCH
-						END
-						ELSE
-							SET @delete_personnel_permission_response = 'Unauthorized to delete permission'
-				END
-				ELSE
-					SET @delete_personnel_permission_response = 'Personnel does not have this permission'
-		END
-		ELSE
-			SET @delete_personnel_permission_response = 'Permission does not exist'
+		SET @response = 'Username does not exist'
+		RETURN (1)
 	END
-	ELSE
-		SET @delete_personnel_permission_response = 'Username does not exist'
+
+
+	-- check for existence of permission in the db to make sure it is a valid permission
+	IF NOT EXISTS(SELECT permissions.id FROM permissions
+				INNER JOIN dbo.permission_actions ON dbo.permissions.action_id = dbo.permission_actions.id
+				INNER JOIN dbo.permission_objects ON dbo.permissions.object_id = dbo.permission_objects.id
+				WHERE dbo.permission_actions.action = @action 
+				AND dbo.permission_objects.object = @object)
+	BEGIN
+		SET @response = 'Permission does not exist'
+		RETURN (2)
+	END
+
+
+	-- only delete permission from @username if @username has the [@action-@object] permission in the first place
+	DECLARE @personnel_has_permission_response nvarchar(3)
+
+	EXEC dbo.uspCheckPersonnelPermission
+		@username = @username,
+		@action = @action,
+		@object = @object,
+		@response = @personnel_has_permission_response OUTPUT
+	IF (@personnel_has_permission_response = 'NO')
+	BEGIN
+		SET @response = 'Personnel does not have this permission'
+		RETURN (3)
+	END
+
+
+	-- check if @auth has the authority to delete personnel
+	DECLARE @check_permissions_response nvarchar(3)
+	EXEC dbo.uspCheckPersonnelPermission
+		@username = @auth,
+		@action = 'delete',
+		@object = 'personnel_permissions',
+		@response = @check_permissions_response OUTPUT
+	IF (@check_permissions_response = 'NO')
+	BEGIN
+		SET @response = 'Unauthorized to delete permission'
+		RETURN (4)
+	END
+
+
+	-- all verifications have been made so starting deleting from the db
+	BEGIN TRY
+		DELETE FROM dbo.personnel_permissions 
+		WHERE personnel_id = (SELECT TOP 1 id FROM dbo.personnel WHERE username=@username) 
+		AND permissions_id = (SELECT TOP 1 id FROM dbo.permissions 
+								WHERE action_id=(SELECT id FROM dbo.permission_actions WHERE action = @action) 
+								AND object_id=(SELECT id FROM dbo.permission_objects WHERE object = @object)
+							 )
+	END TRY
+	BEGIN CATCH
+		SET @response = ERROR_MESSAGE()
+		RETURN (5)
+	END CATCH
+
+
+	-- have to make sure permission is deleted from the database (in case no severe errors raised to push into CATCH block but database did not record properly)
+	EXEC dbo.uspCheckPersonnelPermission
+		@username = @username,
+		@action = @action,
+		@object = @object,
+		@response = @personnel_has_permission_response OUTPUT
+
+	IF (@personnel_has_permission_response = 'YES')
+	BEGIN
+		SET @response = 'Error occurred during the DELETE operation'
+		RETURN (6)
+	END
 	
 
-	-- return response message
-	SET @response = @delete_personnel_permission_response
-	--SELECT @delete_personnel_permission_response as response
+	-- personnel guarenteed to not have permission[@action-@object] in the db anymore so return successful
+	SET @response = 'SUCCESS'
+	RETURN (0)
 END
 GO
 

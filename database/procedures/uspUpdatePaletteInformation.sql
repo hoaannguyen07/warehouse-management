fc9148bba17ca1f00715f3418d7f0c8918ed5b9c
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-06-30 15:14:17 -0500

    Update uspUpdatePaletteInformation.sql
    implemented linear verification format

--- database/procedures/uspUpdatePaletteInformation.sql
+++ database/procedures/uspUpdatePaletteInformation.sql
@@ -20,7 +20,7 @@ GO
 --	@response nvarchar(256) = '' OUTPUT
 
 
--- OUTPUT(S) (by precedence):
+-- OUTPUT(S) (by precedence) (SUCCESS WILL ALWAYS BE '0' BUT BE LOWEST ON THE PRECEDENCE LIST):
 --		1. ERROR: 'Palette ID does not exist' -> id of palette trying to update does not exist in the system
 --		2. ERROR: 'Invalid manufacturing date' -> manufacturing date is not a valid date
 --		3. ERROR: 'Invalid expiration date' -> expiration date is not a valid date
@@ -29,7 +29,8 @@ GO
 --		6. ERROR: 'Invalid type' -> container with name = @type_name and measured in @type_unit does not exist in the db
 --		7. ERROR: 'Unauthorized to update palette information' -> @auth does not have the permission to update palette information
 --		8. ERROR: ERROR_MESSAGE() -> error occurred during the UPDATE operation of that was unaccounted for (but passed all previous checks)
---		9. 'SUCCESS' -> successfully updated the information of palette id = @id
+--		9. ERROR: 'Error occurred during the UPDATE operation' -> something went wrong during the UDPATE operation of the palette but did not cause any errors (or it would've returned ERROR_MESSAGE() instead)
+--		0. 'SUCCESS' -> successfully updated the information of palette id = @id
 
 
 CREATE OR ALTER PROCEDURE [dbo].[uspUpdatePaletteInformation]
@@ -46,159 +47,132 @@ CREATE OR ALTER PROCEDURE [dbo].[uspUpdatePaletteInformation]
 	@amount int,
 	@being_delivered binary,
 	@auth nvarchar(50),
-	@response nvarchar(256) = '' OUTPUT
+	@response nvarchar(256) = NULL OUTPUT
 AS
 BEGIN
 	SET NOCOUNT ON
 
-	DECLARE @update_palette_information_response nvarchar(256) = NULL
-	
-	
 	-- check existence of palette id = @id
 	IF EXISTS (SELECT id FROM dbo.palettes WHERE id = @id)
 	BEGIN
-		DECLARE @converted_manufacturing_date date
-
-		-- try converting manufacturing date to SQL preferred date
-		BEGIN TRY
-			-- convert to string to date, where the string contains a date in the form of code 103 ('DD/MM/YYYY')
-			SET @converted_manufacturing_date = (SELECT CONVERT(date, @manufacturing_date, 103)) -- @converted_manufacturing_date = NULL if @manufacturing)date = NULL w/o errors
-
-			DECLARE @converted_expiration_date date
-
-			-- try converting expiration date to SQL preferred date
-			BEGIN TRY
-				SET @converted_expiration_date = (SELECT CONVERT(date, @expiration_date, 103)) -- @converted_expiration_date = NULL if @expiration_date = NULL w/o errors
-
-				DECLARE @converted_order_date date
-
-				-- try converting order date to SQL preferred date
-				BEGIN TRY
-					SET @converted_order_date = (SELECT CONVERT(date, @order_date, 103)) -- @converted_order_date = NULL if @expiration_date = NULL w/o errors
-
-					-- try converting delviery date to SQL preferred date
-					DECLARE @converted_delivery_date date
-					BEGIN TRY
-						SET @converted_delivery_date = (SELECT CONVERT(date, @delivery_date, 103)) -- @converted_order_date = NULL if @expiration_date = NULL w/o errors
-
-						-- check if type exists
-						IF EXISTS (SELECT TOP 1 id FROM dbo.types WHERE name = @type_name AND unit = @type_unit)
-						BEGIN
-							-- check authorization
-							DECLARE @authorization_response nvarchar(3)
-
-							EXEC dbo.uspCheckPersonnelPermission
-								@username = @auth,
-								@action = 'update',
-								@object = 'palettes',
-								@response = @authorization_response OUTPUT
-
-							IF (@authorization_response = 'YES')
-							BEGIN
-								BEGIN TRY
-									UPDATE dbo.palettes
-									SET product_name = @product_name,
-									manufacturing_date = @converted_manufacturing_date,
-									expiration_date = @converted_expiration_date,
-									order_num = @order_num,
-									order_date = @converted_order_date,
-									delivery_date = @converted_delivery_date,
-									type_id = (SELECT TOP 1 id FROM dbo.types WHERE name = @type_name AND unit = @type_unit),
-									unit_mass = @unit_mass,
-									amount = @amount,
-									total_mass = @unit_mass * @amount,
-									last_modified = GETDATE(),
-									last_modified_by = (SELECT TOP 1 id FROM dbo.personnel WHERE username = @auth),
-									being_delivered = @being_delivered
-									WHERE id = @id
-
-									-- check if the db actuall updated
-									IF EXISTS (SELECT id FROM dbo.palettes
-												WHERE product_name = @product_name
-												AND manufacturing_date = @converted_manufacturing_date
-												AND expiration_date = @converted_expiration_date
-												AND order_num = @order_num
-												AND order_date = @converted_order_date
-												AND delivery_date = @converted_delivery_date
-												AND type_id = (SELECT TOP 1 id FROM dbo.types WHERE name = @type_name AND unit = @type_unit)
-												AND unit_mass = @unit_mass
-												AND amount = @amount
-												AND total_mass = @unit_mass * @amount
-												AND last_modified_by = (SELECT TOP 1 id FROM dbo.personnel WHERE username = @auth)
-												AND being_delivered = @being_delivered
-									)
-									BEGIN
-										SET @update_palette_information_response = 'SUCCESS' 
-									END
-									ELSE
-									BEGIN
-										DECLARE @message nvarchar(256) = NULL
-										SET @message = CONCAT('The UPDATE operation was unable update palette ', @id, '''s information')
-
-										RAISERROR(
-											@message, -- Message
-											16, -- Severity
-											1 -- State
-										)
-									END
-								END TRY
-
-								-- catch any errors that occurred during the update operation
-								BEGIN CATCH
-									SET @update_palette_information_response = ERROR_MESSAGE()
-								END CATCH
-							END
-							ELSE
-							BEGIN
-								SET @update_palette_information_response = 'Unauthorized to update palette information'
-							END
-							-- finished checking authorization of @auth to make sure he/she has the permission to update the palettes table
-						END
-
-						ELSE
-						BEGIN
-							SET @update_palette_information_response = 'Invalid type'
-						END
-						-- finished checking if the inputted type exists
-
-					END TRY
-
-					-- catch errors from converting delivery date
-					BEGIN CATCH
-						SET @update_palette_information_response = 'Invalid delivery date'
-					END CATCH
-					-- finished attempt to convert delivery date
-
-				END TRY
-
-				-- catch errors from converting order date
-				BEGIN CATCH
-					SET @update_palette_information_response = 'Invalid order date'
-				END CATCH 
-				-- finished attempt to convert order date
-			END TRY
-
-			-- catch errors from converting expiration date
-			BEGIN CATCH
-				SET @update_palette_information_response = 'Invalid expiration date'
-			END CATCH 
-			-- finished attempt to convert expiration date
-
-		END TRY
-
-		-- catch errors from converting manufacturing date
-		BEGIN CATCH 
-			SET @update_palette_information_response = 'Invalid manufacturing date'
-		END CATCH
-		-- finished attempt to convert manufacturing date
-
+		SET @response = 'Palette ID does not exist'
+		RETURN (1)
 	END
-	-- palette id = @id does NOT exist in the db
-	ELSE 
-	BEGIN
-		SET @update_palette_information_response = 'Palette ID does not exist'
-	END -- finished checking existence of @id in the db
 
-	SET @response = @update_palette_information_response
-	--SELECT @response AS response
+	-- try converting all dates to make sure that the inputted dates are valid
+	DECLARE @converted_manufacturing_date date
+	DECLARE @converted_expiration_date date
+	DECLARE @converted_order_date date
+	DECLARE @converted_delivery_date date
+
+	-- try converting manufacturing date to SQL preferred date
+	BEGIN TRY
+		-- convert to string to date, where the string contains a date in the form of code 103 ('DD/MM/YYYY')
+		SET @converted_manufacturing_date = (SELECT CONVERT(date, @manufacturing_date, 103)) -- @converted_manufacturing_date = NULL if @manufacturing)date = NULL w/o errors
+	END TRY
+	BEGIN CATCH 
+		SET @response = 'Invalid manufacturing date'
+		RETURN (2)
+	END CATCH
+
+	-- try converting expiration date to SQL preferred date
+	BEGIN TRY
+		SET @converted_expiration_date = (SELECT CONVERT(date, @expiration_date, 103)) -- @converted_expiration_date = NULL if @expiration_date = NULL w/o errors
+	END TRY
+	BEGIN CATCH
+		SET @response = 'Invalid expiration date'
+		RETURN (3)
+	END CATCH 
+
+	-- try converting order date to SQL preferred date
+	BEGIN TRY
+		SET @converted_order_date = (SELECT CONVERT(date, @order_date, 103)) -- @converted_order_date = NULL if @expiration_date = NULL w/o errors
+	END TRY
+	BEGIN CATCH
+		SET @response = 'Invalid order date'
+		RETURN (4)
+	END CATCH 
+	
+	-- try converting delviery date to SQL preferred date
+	BEGIN TRY
+		SET @converted_delivery_date = (SELECT CONVERT(date, @delivery_date, 103)) -- @converted_order_date = NULL if @expiration_date = NULL w/o errors
+	END TRY
+	BEGIN CATCH
+		SET @response = 'Invalid delivery date'
+		RETURN (5)
+	END CATCH
+
+
+	-- check if type exists
+	IF NOT EXISTS (SELECT TOP 1 id FROM dbo.types WHERE name = @type_name AND unit = @type_unit)
+	BEGIN
+		SET @response = 'Invalid type'
+		RETURN (6)
+	END
+
+
+	-- check authorization
+	DECLARE @authorization_response nvarchar(3)
+	EXEC dbo.uspCheckPersonnelPermission
+		@username = @auth,
+		@action = 'update',
+		@object = 'palettes',
+		@response = @authorization_response OUTPUT
+	-- must have permission to update palettes in order to continue
+	IF (@authorization_response = 'NO')
+	BEGIN
+		SET @response = 'Unauthorized to update palette information'
+		RETURN (7)
+	END
+
+	-- all verifications have been made so starting updating palette info in the db
+	BEGIN TRY
+		UPDATE dbo.palettes
+		SET product_name = @product_name,
+		manufacturing_date = @converted_manufacturing_date,
+		expiration_date = @converted_expiration_date,
+		order_num = @order_num,
+		order_date = @converted_order_date,
+		delivery_date = @converted_delivery_date,
+		type_id = (SELECT TOP 1 id FROM dbo.types WHERE name = @type_name AND unit = @type_unit),
+		unit_mass = @unit_mass,
+		amount = @amount,
+		total_mass = @unit_mass * @amount,
+		last_modified = GETDATE(),
+		last_modified_by = (SELECT TOP 1 id FROM dbo.personnel WHERE username = @auth),
+		being_delivered = @being_delivered
+		WHERE id = @id
+	END TRY
+	BEGIN CATCH
+		SET @response = ERROR_MESSAGE()
+		RETURN (8)
+	END CATCH
+
+
+	-- check if the db actuall updated
+	IF EXISTS (SELECT id FROM dbo.palettes
+				WHERE product_name = @product_name
+				AND manufacturing_date = @converted_manufacturing_date
+				AND expiration_date = @converted_expiration_date
+				AND order_num = @order_num
+				AND order_date = @converted_order_date
+				AND delivery_date = @converted_delivery_date
+				AND type_id = (SELECT TOP 1 id FROM dbo.types WHERE name = @type_name AND unit = @type_unit)
+				AND unit_mass = @unit_mass
+				AND amount = @amount
+				AND total_mass = @unit_mass * @amount
+				AND last_modified_by = (SELECT TOP 1 id FROM dbo.personnel WHERE username = @auth)
+				AND being_delivered = @being_delivered
+	)
+	BEGIN
+		-- palette was succesfully updated to return successful
+		SET @response = 'SUCCESS'
+		RETURN (0)
+	END
+	
+
+	-- unable to find the updated entry of the paletteo so the UPDATE operation was unsuccessful
+	SET @response = 'Error occurred during the UPDATE operation'
+	RETURN (9)
 END
+GO
